235a2ec314438bc636eaa08d21e5ba634bb8d6e9
bhavana-2508 <116941797+bhavana-2508@users.noreply.github.com>
2024-12-07 13:03:10 +0530

    Update ClientController.java

--- ClientController.java
+++ ClientController.java
@@ -25,10 +25,10 @@ public class ClientController {
         return "demo1";
     }
 
-    @RequestMapping("/demo2/{num1}/{num2}")
-    public String demo2(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model) {
-        int subtraction = num1 - num2;
-        double division = num2 != 0 ? (double) num1 / num2 : 0;
+    @RequestMapping("/demo2/{x}/{y}")
+    public String demo2(@RequestParam("x") int num1, @RequestParam("y") int num2, Model model) {
+        int subtraction = x - y;
+        double division = y != 0 ? (double) x / y : 0;
         model.addAttribute("subtraction", subtraction);
         model.addAttribute("division", division);
         return "demo2";
@@ -40,25 +40,25 @@ public class ClientController {
     }
 
     @RequestMapping("/multiplyNumbers")
-    public String multiply(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model) {
-        int result = num1 * num2;
+    public String multiply(@RequestParam("x") int x, @RequestParam("y") int y, Model model) {
+        int result = x * y;
         model.addAttribute("result", result);
         return "multiplyNumbers";
     }
 
     @RequestMapping("/reverse")
-    public String reverse(@RequestParam("str1") String str1, @RequestParam("str2") String str2, Model model) {
-        String reversedStr1 = new StringBuilder(str1).reverse().toString();
-        String reversedStr2 = new StringBuilder(str2).reverse().toString();
-        model.addAttribute("reversedStr1", reversedStr1);
-        model.addAttribute("reversedStr2", reversedStr2);
+    public String reverse(@RequestParam("string1") String string1, @RequestParam("string2") String string2, Model model) {
+        String reversedString1 = new StringBuilder(string1).reverse().toString();
+        String reversedString2 = new StringBuilder(string2).reverse().toString();
+        model.addAttribute("reversedString1", reversedString1);
+        model.addAttribute("reversedString2", reversedString2);
         return "reverse";
     }
 
     @RequestMapping("/calculate")
-    public String calculate(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model) {
-        int addition = num1 + num2;
-        int subtraction = num1 - num2;
+    public String calculate(@RequestParam("x") int x, @RequestParam("y") int y, Model model) {
+        int addition = x + y;
+        int subtraction = x - y;
         model.addAttribute("addition", addition);
         model.addAttribute("subtraction", subtraction);
         return "calculate";
